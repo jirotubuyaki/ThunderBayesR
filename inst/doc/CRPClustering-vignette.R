9e7be521831bf5ac6c6b749f27a24af0d83836f1
jirotubuyaki <okadaalgorithm@gmail.com>
2018-05-02 07:50:21 +0900

    version 1.3 release.

--- inst/doc/CRPClustering-vignette.R
+++ inst/doc/CRPClustering-vignette.R
@@ -1,6 +1,1 @@
-## ----fig.width=13.0,fig.height=4.0,fig.align="center",fig.cap="CRP clustering result",echo=FALSE----
-library(png)
-library(grid)
-img <- readPNG("./image.png")
- grid.raster(img)
-
+### This is an R script tangled from 'CRPClustering-vignette.pdf.asis'
